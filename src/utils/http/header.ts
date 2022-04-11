eee71ba52e4089df4864aae853fe2d5801eb068d
willy <willy8742891@gmail.com>
2022-04-11 23:48:06 +0800

    add IHeader.

---
+++ src/utils/http/header.ts
@@ -0,0 +1,29 @@
+import { IterablePrototype } from '../common';
+
+export default class IHeaders extends IterablePrototype {
+  constructor(args?: HeadersInit | IHeaders) {
+    super();
+  }
+
+  static resolve(headers: HeadersInit | IHeaders | Record<string, any>): Record<string, string> {
+    let result: Record<string, string> = {}
+    if (headers instanceof Headers || headers instanceof IHeaders) {
+      headers.forEach((key, value) => {
+        result[key] = value
+      })
+    } else if (headers instanceof Array) {
+      headers.forEach(([key, value]) => {
+        result[key] = value
+      })
+    } else {
+      Object.keys(headers).forEach(key => {
+        result[key] = typeof headers[key] === 'object' ? JSON.stringify(headers[key]) : headers[key]
+      })
+    }
+    return result
+  }
+
+  static createHeader(headers: HeadersInit) {
+    return new IHeaders(headers)
+  }
+}
